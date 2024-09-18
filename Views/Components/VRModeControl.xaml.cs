a9fe4c957709ec5bd19868202fbf9458384c6f16
Andrei Ghiriti <95663488+DroptheKraken@users.noreply.github.com>
2024-09-18 14:51:16 +0200

    Added connection to VR switch

--- Views/Components/VRModeControl.xaml.cs
+++ Views/Components/VRModeControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -27,5 +28,30 @@ public partial class VRModeControl : UserControl
         {
             VRModeToggle.Background = Brushes.Red;
         }
+        SendMessageToUnity("spawn");
     }
+
+    public void SendMessageToUnity(string message)
+    {
+        try
+        {
+            Console.WriteLine("Attempting to connect to Unity...");
+            using (TcpClient client = new TcpClient("127.0.0.1", 13000)) // Ensure the IP and port match Unity's listener
+            {
+                Console.WriteLine("Connected to Unity!");
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Message sent to Unity: " + message);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to send message: " + ex.Message);
+        }
+    }
+
+    
 }
