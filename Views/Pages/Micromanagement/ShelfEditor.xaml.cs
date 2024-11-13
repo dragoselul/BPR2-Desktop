1c5ca5fb14c11953c1f7493732f931a8a2069564
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-11-13 16:47:20 +0100

    DW-59
    Made a micromanagement window with working database fetch and search functionality for the sidepanel

---
+++ Views/Pages/Micromanagement/ShelfEditor.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace BPR2_Desktop.Views.Pages.Micromanagement;
+
+public partial class ShelfEditor : Page
+{
+    public ShelfEditor()
+    {
+        InitializeComponent();
+    }
+}
