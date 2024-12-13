17b1eaf39fba3832fabb52d8aa148f2aac5e16d0
Andrei Ghiriti <95663488+DroptheKraken@users.noreply.github.com>
2024-12-13 11:53:10 +0100

    Modified input fields

--- ViewModels/MicroManagement/ProductViewModel.cs
+++ ViewModels/MicroManagement/ProductViewModel.cs
@@ -1,32 +1,14 @@
-﻿using BPR2_Desktop.Database;
+﻿using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using BPR2_Desktop.Database;
 using BPR2_Desktop.Model;
-using BPR2_Desktop.ViewModels.UserControls;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace BPR2_Desktop.ViewModels.MicroManagement;
 
-public partial class ProductViewModel : ViewModel
+public class ProductViewModel : ViewModel
 {
-    [ObservableProperty]
-    private ItemListViewModel _itemListViewModel;
-
-    [ObservableProperty]
-    private ImageDisplayPanelViewModel _imageDisplayViewModel;
     
-    public ProductViewModel(ProductContext context)
-    {
-        _itemListViewModel = new ItemListViewModel(context, OnItemSelected);
-        _imageDisplayViewModel = new ImageDisplayPanelViewModel(context);
-    }
-    
-    private void OnItemSelected(Product selectedProduct)
-    {
-        if (selectedProduct == null)
-        {
-            ImageDisplayViewModel.ClearImage();
-            return;
-        }
-
-        // Call a method in ImageDisplayPanelViewModel
-        ImageDisplayViewModel.LoadImage(selectedProduct.Main_EAN);
-    }
 }
